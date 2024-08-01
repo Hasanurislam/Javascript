8bca2d88aac7e2759edca7dda7094b9858221b77
hasanurislam <hasanur7811@gmail.com>
2024-08-01 21:38:18 +0530

    String and function

--- hasanur.js
+++ hasanur.js
@@ -1,8 +1,16 @@
-let n=10;
-
-let i=1;
-while(i<n)
-{
-    console.log(i)
-    i++;
+/*const hello=()=>{
+    console.log("hey how are you");
+    return "hi";
 }
+let v=hello()
+console.log(v)
+*/
+/*let name="Hasanur"
+console.log(name[0])
+let m='zulfikar'
+console.log(m[4])*/
+
+let boy1=`Hasanur`
+let boy2=`Zulfikar`
+let sentance=(`${boy2} is a friend of ${boy1}`);
+console.log(sentance)
