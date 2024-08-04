78a064f6ef89f0924915fd79a38249263524643b
hasanurislam <hasanur7811@gmail.com>
2024-08-04 17:27:09 +0530

    Array in loop

---
+++ ArraysInLoop.js
@@ -0,0 +1,18 @@
+let num=[3,5,7,8,2];
+//for(let i=0;i<num.length;i++)
+//{
+//    console.log(num[i])
+//}
+//for Each loop in array
+//num.forEach(element => {
+ //   console.log(element*element)
+    
+//});
+let name1="Hasanur"
+let arr=Array.from(name1);
+console.log(arr)
+
+for (let i of num) {
+    console.log(i)
+    
+}
