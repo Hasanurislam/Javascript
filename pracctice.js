4aeb836c280685ed551558581e28976a97500ca3
hasanurislam <hasanur7811@gmail.com>
2024-09-04 16:05:09 +0530

    loops

---
+++ pracctice.js
@@ -0,0 +1,13 @@
+let obj={
+    hasanur:70,
+    Zulfikar:50,
+    harry:60,
+    }
+    for(let a in obj)
+    {
+        console.log("marks of " +a +" are " +obj[a]);
+    }
+    for(let b of 'hasanur')
+    {
+        console.log(b);
+    }
