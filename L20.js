5131d9eb00acee1346f9bbef7d22548d3db6448b
hasanurislam <hasanur7811@gmail.com>
2024-09-06 23:32:53 +0530

    condition check

--- L20.js
+++ L20.js
@@ -1,6 +1,24 @@
-//alert("Hello your script works");
-let a=prompt("Enter your number: ");
-let b=prompt("Enter your second number: ");
-a=Number.parseInt(a);
-b=Number.parseInt(b);
-console.log("you entered a type of "+(typeof a));
+let runagain=true;
+
+while(runagain)
+{
+    let age=prompt("Enter your age: ");
+    age=parseInt(age);
+    if(age<=0)
+    {
+        console.error("please enter a valid age");
+        break;
+    }
+    let candrive=(age)=>{
+       return age>=18?true:false;
+    }
+    if(candrive(age))
+    {
+        alert("You can drive");
+    }
+    else
+    {
+        alert("You can not drive:");
+    }
+    runagain=confirm("Can you again enter your age");
+}
