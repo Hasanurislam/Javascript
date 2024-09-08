40f01f906ef47a07dc515ffad49f7bebbcb0b5a1
hasanurislam <hasanur7811@gmail.com>
2024-09-08 20:46:52 +0530

    elements only navigation

---
+++ L22.js
@@ -0,0 +1,6 @@
+const changebody=()=>{
+  document.body.firstElementChild.style.background="red"
+}
+let b=document.body
+console.log("the first child of b :", b.firstChild);
+console.log("the first Element child of b :", b.firstElementChild);
