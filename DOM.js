97551dbf480faf6330dc95b40e15c541ff254895
hasanurislam <hasanur7811@gmail.com>
2024-10-05 14:19:50 +0530

    DOM

--- DOM.js
+++ DOM.js
@@ -1,5 +1,16 @@
 //
 
-let headings=document.getElementsByClassName("heading");
+/*let headings=document.getElementsByClassName("heading");
 console.dir(headings);
 console.log(headings);
+let para=document.getElementsByTagName("p");
+console.dir(para);
+*/
+//let para2=document.querySelector("p");
+//console.dir(para2);
+//to select all values
+//let elements=document.querySelectorAll("p");
+//console.dir(elements);
+let fruits=document.querySelector("div");
+console.dir(fruits);
+let heading=document.querySelector("h1");
