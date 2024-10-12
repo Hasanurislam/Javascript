8a0b62b67324cbd045d4c1e0d67e5a678db1b861
hasanurislam <hasanur7811@gmail.com>
2024-10-12 13:11:35 +0530

    mini project Tudo App

--- EventListener.js
+++ EventListener.js
@@ -1,4 +1,4 @@
-let btn=document.querySelector("button");
+/*let btn=document.querySelector("button");
 
 btn.addEventListener("click",function(){
     let h1=document.querySelector("h1");
@@ -18,3 +18,37 @@ function randomcolor(){
     let color=`rgb(${red},${green},${blue})`;
     return color
 }
+    */
+/*let btn=document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    console.log(this.innerHTML);
+    this.style.backgroundColor="red";
+})
+    */
+   /*let inp=document.querySelector("input");
+
+   inp.addEventListener("keyup",function(event){
+    //
+    if(event.code=="ArrowUp"){
+        console.log("the character moves fordward");
+    }
+    else if(event.code=="ArrowDown"){
+        console.log("the character moves backward");
+    }
+    else if(event.code=="ArrowRight"){
+        console.log("the character moves Right");
+    }
+    else if(event.code=="ArrowLeft"){
+        console.log("the character moves left");
+    }
+   });
+   */
+  let form=document.querySelector("form");
+
+  form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let inp=document.querySelector("input");
+    console.dir(inp.value);
+
+  });
