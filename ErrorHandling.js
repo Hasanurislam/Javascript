979688d3580805251f64f2ef4de5ab90ecd57c0e
hasanurislam <hasanur7811@gmail.com>
2024-10-18 22:40:04 +0530

    Error handling in javascript

---
+++ ErrorHandling.js
@@ -0,0 +1,23 @@
+setTimeout(()=>{
+    console.log("haking wifi.....please wait......");
+
+},1000);
+
+try{
+    console.log(hasanur);
+}
+catch(error){
+    console.log("woooooohoooo")
+}
+
+
+
+setTimeout(()=>{
+    console.log("Fatching passward.....please wait......");
+
+},2000);
+setTimeout(()=>{
+    console.log("haking hasanur instagram id.....please wait......");
+
+},3000);
+
